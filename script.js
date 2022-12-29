3525f550619a0374f3cccf65c0944f688c45c88f
kpatel115 <kp9338448@gmail.com>
2022-12-29 09:18:25 -0600

    Working on Edit + Delete button functionality

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ let input = document.getElementById("input");
 let button = document.getElementById("btn");
 let list = document.getElementById("list");
 
-const InputValue = () => {
+const CreatePost = () => {
   let listItem = input.value;
-  list.innerHTML = listItem; 
+  list.innerHTML += `
+  <p>${listItem}
+  <button type="button" onclick="EditPost()">Edit</button>
+  <button type="button" onclick="DeletePost()">Delete</button>
+  </p>
+  `; 
+}
+
+const EditPost = () => {
+  let listItem = input.value;
+  console.log("Edit");
+  let edit = prompt("Enter your edit");
+  listItem.innerHTML += edit;
+
+}
+
+const DeletePost = () => {
+  let listItem = input.value;
+  console.log("Delete");
+  listItem.innerHTML = ""
 }
