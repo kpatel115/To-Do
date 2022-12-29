850683bb445c0a41727a88c1af320425d0976dfb
kpatel115 <kp9338448@gmail.com>
2022-12-29 09:55:43 -0600

    Delete Button partially working

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let list = document.getElementById("list");
 const CreatePost = () => {
   let listItem = input.value;
   list.innerHTML += `
-  <p>${listItem}
+  <p id="postItem">${listItem}
   <button type="button" onclick="EditPost()">Edit</button>
   <button type="button" onclick="DeletePost()">Delete</button>
   </p>
@@ -14,14 +14,18 @@ const CreatePost = () => {
 
 const EditPost = () => {
   let listItem = input.value;
+  let post = document.getElementById("post");
   console.log("Edit");
   let edit = prompt("Enter your edit");
-  listItem.innerHTML += edit;
-
-}
+  if(edit != ""){
+  listItem.innerHTML = edit;
+  } else {
+    alert("enter a valid edit");
+  };
+};
 
 const DeletePost = () => {
   let listItem = input.value;
+  list.innerText = ""; 
   console.log("Delete");
-  listItem.innerHTML = ""
 }
