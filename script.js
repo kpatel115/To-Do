c117b2babd4f90c092094196d91b240d1ac3dda2
kpatel115 <kp9338448@gmail.com>
2022-12-30 14:53:37 -0600

    Added form validation and enter triggers submit

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
+// Global Variables for input form and todo list
 let input = document.getElementById("input");
 let button = document.getElementById("btn");
 let list = document.getElementById("list");
 
+ // Submits on keypress
+ input.addEventListener("keydown", function(e) {
+   if (e.key === "Enter"){
+    button.click();
+    input.value === "";
+    e.preventDefault();
+   }
+ })
+
+// Creates post 
 const CreatePost = () => {
   let listItem = input.value;
+  if(listItem === ""){
+    alert('Please enter valid input!');
+  } else {
   list.innerHTML += `
-  <p id="postItem">${listItem}
+  <div id="post">
+  <p id="postItem">${listItem}</p>
   <button type="button" onclick="EditPost()">Edit</button>
   <button type="button" onclick="DeletePost()">Delete</button>
-  </p>
+  </div>
   `; 
-}
+  preventDefault()
+  }};
 
+// Edits post
 const EditPost = () => {
-  let listItem = input.value;
-  let post = document.getElementById("post");
-  console.log("Edit");
-  let edit = prompt("Enter your edit");
-  if(edit != ""){
-  listItem.innerHTML = edit;
+  let post = document.getElementById("postItem");
+  let editToPost = prompt("Enter your edit!");
+  if (editToPost === ""){
+    alert("Please enter an edit!");
   } else {
-    alert("enter a valid edit");
-  };
+    post.innerHTML = editToPost;
+    console.log("user post assigned to edit");
+  }
 };
 
+// Deletes post
 const DeletePost = () => {
   let listItem = input.value;
   list.innerText = ""; 
