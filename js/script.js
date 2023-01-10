79bce8248cb894048fd0fb9669062158b3385c96
kpatel115 <kp9338448@gmail.com>
2023-01-10 16:07:41 -0600

    organized code tree with folders

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let input_field = document.getElementById("input").value;
+let input_button = document.getElementById("input-button");
+let list = document.getElementById("list");
+
+function AddItemToList(){
+  if(input_field === ""){
+    console.log("add a list item");
+  } else {
+    input_field
+  }
+}
+function EditItemToList(){
+
+}
+function DeleteItemFromList(){
+
+}
